9a3a15f774bc63ab862fd2dbfad862abfba1b189
ashu@ZenUbuntu <ashu@example.com>
2025-07-27 15:55:51 +0530

    WIP 5:21:22

--- client/lib/core/utils.dart
+++ client/lib/core/utils.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar({
@@ -15,3 +18,35 @@ void showSnackBar({
       ),
     );
 }
+
+Future<File?> pickAudio() async {
+  try {
+    final filePickerRes =
+        await FilePicker.platform.pickFiles(type: FileType.audio);
+
+    if (filePickerRes != null) {
+      return File(filePickerRes.files.first.xFile.path);
+    }
+
+    return null;
+  } catch (error) {
+    return null;
+  }
+}
+
+Future<File?> pickImage() async {
+  try {
+    final filePickerRes =
+        await FilePicker.platform.pickFiles(type: FileType.image);
+
+    if (filePickerRes != null) {
+      return File(filePickerRes.files.first.xFile.path);
+    }
+
+    return null;
+  } catch (error) {
+    return null;
+  }
+}
+
+// pickImage
